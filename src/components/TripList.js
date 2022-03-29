2624677ca6e31bfb836f091575320c7daa0b5a83
Danielle Kom <daniellekom1@gmail.com>
2022-03-29 11:29:22 -0400

    use effect and fetch

---
+++ src/components/TripList.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+
+//styles
+import './tripList.css'
+
+export default function TripList() {
+ const [trips,setTrips]= useState([])
+ 
+ useEffect(() => {
+     fetch("http://localhost:3000/trips")
+        .then(response => response.json())
+        .then(json =>setTrips(json))
+ }, [])
+    
+  console.log(trips)
+
+
+
+  return (
+    <div className="trip-list">
+        <h2>PACKAGE DEALS</h2>
+        <ul>
+            {trips.map(trip =>(
+                <li key ={trip.id}>
+                    <h3>{trip.title}</h3>
+                    <p>{trip.price}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
